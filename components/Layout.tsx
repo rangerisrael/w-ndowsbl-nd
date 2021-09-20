f794052234524127bc011f0e8cba83ecde10c9df
Israel <israelalisoso1@gmail.com>
2021-09-20 06:00:24 -0700

    loading spinner using material-ui

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { ReactChildren, ReactElement, useContext } from 'react';
-import { AppBar, Container, Link, Toolbar, Typography, Switch } from '@mui/material';
+import React, { ReactChildren, ReactElement, useContext, useState, useEffect } from 'react';
+import { AppBar, Container, Link, Toolbar, Typography, Switch, CircularProgress, Box } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Cookies from 'js-cookie';
@@ -41,6 +41,8 @@ const LayoutStyle = styled.div`
 // eslint-disable-next-line no-undef
 export default function Layout({ titles, children }: LayoutProps) {
   const { state, dispatch } = useContext(Store);
+  const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(10);
   const { darkMode } = state;
 
   // Create a theme instance.
@@ -77,6 +79,18 @@ export default function Layout({ titles, children }: LayoutProps) {
     Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 2000);
+    const timer = setInterval(() => {
+      setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
+    }, 200);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <LayoutStyle>
       <Head>
@@ -84,38 +98,70 @@ export default function Layout({ titles, children }: LayoutProps) {
         <link href="/favicon.ico" rel="icon" />
         <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
       </Head>
+
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <AppBar className="appBar" position="static">
-          <Toolbar>
-            <NextLink href="/">
-              <Link sx={{ color: '#f3f4f6' }}>
-                <a>
-                  <Typography sx={{ fontSize: '1.5rem', fontWeight: 'bold' }}>W!ndowsBl!nd</Typography>
-                </a>
-              </Link>
-            </NextLink>
+        {!loading ? (
+          <Box sx={{ position: 'relative', background: 'transparent' }}>
+            <CircularProgress
+              thickness={1.5}
+              size={100}
+              sx={{
+                margin: 'auto',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+              }}
+              value={progress}
+            />
+            <Typography
+              sx={{
+                height: '100vh',
+                display: 'flex', // make us of Flexbox
+                alignItems: 'center', // does vertically center the desired content
+                justifyContent: 'center', // horizontally centers single line items
+                textAlign: 'center', // optional, but helps horizontally center text that breaks into multiple lines
+              }}
+            >
+              {progress}%
+            </Typography>
+          </Box>
+        ) : (
+          <>
+            <AppBar className="appBar" position="static">
+              <Toolbar>
+                <NextLink href="/">
+                  <Link sx={{ color: '#f3f4f6' }}>
+                    <a>
+                      <Typography sx={{ fontSize: '1.5rem', fontWeight: 'bold' }}>W!ndowsBl!nd</Typography>
+                    </a>
+                  </Link>
+                </NextLink>
 
-            <div style={{ flexGrow: 1 }} />
-            <div>
-              <Switch checked={darkMode} onChange={darkModeChangeHandler} />
-              &nbsp;
-              <NextLink href="/cart" passHref>
-                <Link>Cart</Link>
-              </NextLink>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <NextLink href="/login" passHref>
-                <Link>Login</Link>
-              </NextLink>
-            </div>
-          </Toolbar>
-        </AppBar>
-        <Container className="container">{children}</Container>
-        <footer className="footer">
-          <Typography component="h2" variant="h6" sx={{ color: '#f3f4f6' }}>
-            All right reserved &copy; 2021 <strong>w!ndowsbl!nd@shop</strong>{' '}
-          </Typography>
-        </footer>
+                <div style={{ flexGrow: 1 }} />
+                <div>
+                  <Switch checked={darkMode} onChange={darkModeChangeHandler} />
+                  &nbsp;
+                  <NextLink href="/cart" passHref>
+                    <Link>Cart</Link>
+                  </NextLink>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <NextLink href="/login" passHref>
+                    <Link>Login</Link>
+                  </NextLink>
+                </div>
+              </Toolbar>
+            </AppBar>
+            <Container className="container">{children}</Container>
+            <footer className="footer">
+              <Typography component="h2" variant="h6" sx={{ color: '#f3f4f6' }}>
+                All right reserved &copy; 2021 <strong>w!ndowsbl!nd@shop</strong>{' '}
+              </Typography>
+            </footer>
+          </>
+        )}
       </ThemeProvider>
     </LayoutStyle>
   );
