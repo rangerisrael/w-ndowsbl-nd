811ddbfa2b830a60c38576fd32ef82f46ca2d530
Israel <israelalisoso1@gmail.com>
2021-10-25 14:41:37 +0800

    Test redirect problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,41 +61,34 @@ export default function Index({ products }: Props) {
         <Typography variant="h4" component="h1" gutterBottom>
           Product
         </Typography>
-        {products && (
-          <Grid container spacing={3}>
-            {products.map((product) => (
-              <Grid item md={4} key={product.name}>
-                <Card>
-                  <NextLink href={`/product/${product.slug}`} passHref>
-                    <CardActionArea>
-                      <CardMedia component="img" image={product.image} title={product.name} />
-                      <CardContent
-                        sx={{
-                          display: 'flex',
-                          justifyContent: 'space-around',
-                          color: '#f3f4f6',
-                          background: '#506E7F',
-                        }}
-                      >
-                        <Typography variant="h6" component="h1">
-                          &#8369;{product.price}
-                        </Typography>
-                        <Typography variant="h5" component="h1">
-                          {product.name}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </NextLink>
-                  <CardActions sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                    <Button fullWidth variant="contained" onClick={() => addToCartHandler(product)}>
-                      Add to Basket
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        )}
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia component="img" image={product.image} title={product.name} />
+                    <CardContent
+                      sx={{ display: 'flex', justifyContent: 'space-around', color: '#f3f4f6', background: '#506E7F' }}
+                    >
+                      <Typography variant="h6" component="h1">
+                        &#8369;{product.price}
+                      </Typography>
+                      <Typography variant="h5" component="h1">
+                        {product.name}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
+                <CardActions sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                  <Button fullWidth variant="contained" onClick={() => addToCartHandler(product)}>
+                    Add to Basket
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
     </Layout>
   );
