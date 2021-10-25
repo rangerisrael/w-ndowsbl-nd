c94825432698c5b6baad6699ebd3e877f18cbeba
Israel <israelalisoso1@gmail.com>
2021-10-25 15:29:25 +0800

    Fixing error

--- pages/api/users/signIn.tsx
+++ pages/api/users/signIn.tsx
@@ -17,26 +17,25 @@ handler.post(async (_req: NextApiRequest, res: NextApiResponse) => {
       res.send({ message: 'This is your previous password' });
     }
     if (bcrypt.compareSync(_req.body.password, users.password)) {
-
-     if(!users.verify) {
-      await db.disconnect();
-      res.send({ message: 'Email is not verified', verify: false, id: users._id });
-    } else {
-      const token = signToken(users);
-      res.send({
-        token,
-        _id: users._id,
-        name: users.name,
-        email: users.email,
-        role: users.role,
-        verify: users.verify,
-        message: 'Access Granted',
-        id: users._id,
-      });
-      await db.disconnect();
-      res.send({ message: 'Access Granted', verify: true, id: users._id });
-    } 
-    
+      if (!users.verify) {
+        await db.disconnect();
+        res.send({ message: 'Email is not verified', verify: false, id: users._id });
+      } else {
+        const token = signToken(users);
+        res.send({
+          token,
+          _id: users._id,
+          name: users.name,
+          email: users.email,
+          role: users.role,
+          verify: users.verify,
+          message: 'Access Granted',
+          id: users._id,
+        });
+        await db.disconnect();
+        res.send({ message: 'Access Granted', verify: true, id: users._id });
+      }
+    }
   } else {
     await db.disconnect();
     res.send({ message: 'Invalid credentials' });
