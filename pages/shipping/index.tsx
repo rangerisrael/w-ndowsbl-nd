20a12537933ea671417b2c82d08e588ae7e44a5c
Israel <israelalisoso1@gmail.com>
2021-10-25 14:30:42 +0800

    debugging code

--- pages/shipping/index.tsx
+++ pages/shipping/index.tsx
@@ -17,11 +17,8 @@ export default function ShippingAddress() {
   const { userInfo } = state;
 
   useEffect(() => {
-    if (userInfo) {
-      router.push('/login?redirect=/shipping');
-    }
     if (!userInfo) {
-      router.push('/login');
+      router.push('/login?redirect=/shipping');
     }
   }, [router, userInfo]);
 
