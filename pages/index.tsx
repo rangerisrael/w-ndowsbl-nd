5cc8a19f66ef18743b3c26ae84edcc1a9978fd41
Israel <israelalisoso1@gmail.com>
2021-10-25 17:51:59 +0800

    test bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,41 +56,39 @@ export default function Index({ products }: Props) {
   };
 
   return (
-    <Layout titles="list-item">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Product
-        </Typography>
-        <Grid container spacing={3}>
-          {products.map((product) => (
-            <Grid item md={4} key={product.name}>
-              <Card>
-                <NextLink href={`/product/${product.slug}`} passHref>
-                  <CardActionArea>
-                    <CardMedia component="img" image={product.image} title={product.name} />
-                    <CardContent
-                      sx={{ display: 'flex', justifyContent: 'space-around', color: '#f3f4f6', background: '#506E7F' }}
-                    >
-                      <Typography variant="h6" component="h1">
-                        &#8369;{product.price}
-                      </Typography>
-                      <Typography variant="h5" component="h1">
-                        {product.name}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </NextLink>
-                <CardActions sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                  <Button fullWidth variant="contained" onClick={() => addToCartHandler(product)}>
-                    Add to Basket
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </Layout>
+    <Box sx={{ my: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Product
+      </Typography>
+      <Grid container spacing={3}>
+        {products.map((product) => (
+          <Grid item md={4} key={product.name}>
+            <Card>
+              <NextLink href={`/product/${product.slug}`} passHref>
+                <CardActionArea>
+                  <CardMedia component="img" image={product.image} title={product.name} />
+                  <CardContent
+                    sx={{ display: 'flex', justifyContent: 'space-around', color: '#f3f4f6', background: '#506E7F' }}
+                  >
+                    <Typography variant="h6" component="h1">
+                      &#8369;{product.price}
+                    </Typography>
+                    <Typography variant="h5" component="h1">
+                      {product.name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </NextLink>
+              <CardActions sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                <Button fullWidth variant="contained" onClick={() => addToCartHandler(product)}>
+                  Add to Basket
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
